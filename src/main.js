06b4cb52f52c6760f24027d5c572bf331e2656e6
Brock <brock.reece@croud.co.uk>
2017-09-10 11:22:55 +0200

    Move require statements to imports

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import FullCalendar from 'vue-full-calendar';
+
 import App from './App';
 
 Vue.config.productionTip = false;
-Vue.use(require('vue-full-calendar'));
+Vue.use(FullCalendar);
 
 /* eslint-disable no-new */
 new Vue({
